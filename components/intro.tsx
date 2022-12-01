0c2570da8c6a9fa08a8a07d31321a5f8cf02286e
Abu <harisishar@gmail.com>
2022-12-01 11:06:00 +0800

    config update

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,22 +4,23 @@ export default function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-6xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Blog.
+        Journal
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
-        A statically generated blog example using{' '}
+        Market Analysis by {' '}
         <a
-          href="https://nextjs.org/"
+          href="https://idw.design/"
           className="underline hover:text-success duration-200 transition-colors"
         >
-          Next.js
+          IDW Design & Build
         </a>{' '}
-        and{' '}
+        for {' '}
         <a
-          href={CMS_URL}
+          href="#/"
           className="underline hover:text-success duration-200 transition-colors"
+          
         >
-          {CMS_NAME}
+          Industires
         </a>
         .
       </h4>
