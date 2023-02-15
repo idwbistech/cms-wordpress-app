09a9d18f093788b35a69ff57f81376c2766f3122
idwbistech <124951173+idwbistech@users.noreply.github.com>
2023-02-15 17:47:10 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 const next = require("next");
 const app = next({});
-if (!process.env.WORDPRESS_API_URL) {
+if (!idw.design / graphql) {
   throw new Error(`
     Please provide a valid WordPress instance URL.
     Add to your environment variables WORDPRESS_API_URL.
@@ -11,7 +11,7 @@ if (!process.env.WORDPRESS_API_URL) {
 module.exports = {
   images: {
     domains: [
-      process.env.idw.design / graphql.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
+      idw.design / graphql.match(/(?!(w+)\.)\w*(?:\w+\.)+\w+/)[0], // Valid WP Image domain.
       "0.gravatar.com",
       "1.gravatar.com",
       "2.gravatar.com",
